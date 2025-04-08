10974ef1bcbd6465401f0194ef44986db9e92143
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2025-04-08 19:47:24 -0300

    Update codigo.py

--- Aula 6/Girafa/codigo.py
+++ Aula 6/Girafa/codigo.py
@@ -1,1 +1,24 @@
+# contorno
+contours, hierarchy = cv2.findContours(
+                                   image = img_close,
+                                   mode = cv2.RETR_TREE,
+                                   method = cv2.CHAIN_APPROX_SIMPLE)
+contours = sorted(contours, key = cv2.contourArea, reverse = True)
+img_copy = img.copy()
+final = cv2.drawContours(img_copy, contours, contourIdx = -1,
+                         color = (255, 0, 0), thickness = 2)
 
+
+#plot imagens
+imagens = [img,img_blur,img_gray,edges_gray,edges_blur,thresh,thresh_open,final]
+formatoX = math.ceil(len(imagens)**.5)
+if (formatoX**2-len(imagens))>formatoX:
+    formatoY = formatoX-1
+else:
+    formatoY = formatoX
+for i in range(len(imagens)):
+    plt.subplot(formatoY, formatoX, i + 1)
+    plt.imshow(imagens[i],'gray')
+    plt.xticks([]),plt.yticks([])
+plt.show()
+plt.imshow(final)
